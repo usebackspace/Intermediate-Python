c53f4a805b24ad82036492374f2296c9b00bb468
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2024-01-12 01:37:29 +0530

    update

--- 24. File Handling/13. copy file.py
+++ 24. File Handling/13. copy file.py
@@ -1,9 +1,9 @@
 print('----- Copying file-----')
-file =  open('Read file.txt', mode='r')
-file1 =  open('Copied file.txt', mode='w')
+file =  open('Read file.txt', mode='r')    # Before reading first we have to read
+file1 =  open('Copied file.txt', mode='w') # After reading we will have to write the read file
 
-content = file.read()
-file1.read(content)
+content = file.read()    # File will be read and return in content.
+file1.write(content)     # Now we will write the read file into file1 object/file handler.
 print(f'{content}') 
 file.close()
 
--- 24. File Handling/14. with statement.py
+++ 24. File Handling/14. with statement.py
@@ -1,8 +1,8 @@
 
-print('----- 1. Reading a file using read() method-----')
+print('----- 1. Reading a file using with statement-----')
 with open('Read file.txt', mode='r') as file:
     content = file.read()
     print(f'{content}')
-    print(file.closed)
+    print(file.closed)   # Will check whether the file is closed or not, return True if file is closed else False.
     
 print(file.closed)
