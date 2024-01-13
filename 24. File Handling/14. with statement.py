ccd34a754a2a686ac89439964a6afd9703c64ede
Sandip Jaiswar <jaiswarsandeep00@gmail.com>
2024-01-13 17:19:33 +0530

    update

--- 24. File Handling/14. with statement.py
+++ 24. File Handling/14. with statement.py
@@ -6,3 +6,19 @@ with open('Read file.txt', mode='r') as file:
     print(file.closed)   # Will check whether the file is closed or not, return True if file is closed else False.
     
 print(file.closed)
+
+
+input_file_path = 'python.txt'
+output_file_path = 'binary_output.bin'
+
+# Read the text file
+with open(input_file_path, 'r', encoding='utf-8') as text_file:
+    text_content = text_file.read()
+
+# Convert the text content to binary
+binary_content = text_content.encode('utf-8')
+print(binary_content)
+
+# Write the binary content to a new file
+with open(output_file_path, 'wb') as binary_file:
+    binary_file.write(binary_content)
--- 24. File Handling/6. Writing data to binary file.py
+++ 24. File Handling/6. Writing data to binary file.py
@@ -1,7 +1,7 @@
 
 # print('----- 1. Creating a  Binary file to write a data with mode="w"-----')
 # file =  open('Binary file.bin', mode='wb')
-# file.write(b'\x48\x65\x6C\x6C\x6F\x2C\x20\x57\x6F\x72\x6C\x64\x21')
+# file.write(b'Hello World')
 # file.close()
 
 # print()
@@ -10,7 +10,7 @@
 # print('----- 2. Executing a same file in same mode="w" -----')
 
 # file =  open('Binary file.bin', mode='wb')
-# file.write(b'\x0A\x42\x79\x74\x65\x73')
+# file.write(b'Hello World')
 # file.close()
 
 print()
@@ -19,7 +19,7 @@ print()
 
 # print('----- 3. Appending a data to a file with mode="a"-----')
 # file =  open('Binary file.bin', mode='ab')
-# file.write(b'\x0A\x42\x79\x74\x65\x73')
+# file.write(b'Hello World')
 # file.close()
 
 print()
@@ -28,7 +28,7 @@ print()
 
 print('----- 4. Exclusive creation of a file with mode="x"-----')
 file =  open('Binary file.bin', mode='xb')
-file.write(b'\x48\x65\x6C\x6C\x6F\x2C\x20\x57\x6F\x72\x6C\x64\x21')
+file.write(b'Hello World!')
 file.close()
 
 # print()
